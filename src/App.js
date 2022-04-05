c4edfd62e21f4d20f9bc39eed7fef218bc7b8d3a
BuildTools <unconfigured@null.spigotmc.org>
2022-04-05 23:07:42 +0200

    ajout de question placeholder afin de montrer le fonctionnement

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import testForm from "./testForm.json";
+import Questions from "./components/Questions";
+console.log(testForm);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container text-center mt-5">
+          {testForm.data.map((question, index) => {
+              return <Questions key={index} question={question}/>
+          })}
+      </div>
   );
 }
 
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,15 +1,33 @@
-function Test(value) {
-    const handleInput = (e) =>{
-        console.log(e.target.value);
+function Questions(props) {
+    var question = props.question.title;
+    const handleInput = (e) => {
+        const id = document.getElementById(props.question[question][0].id)
+        const elements = document.getElementsByClassName("question");
+        for(var i = 0; i < elements.length; i++) {
+            if(elements[i].classList[0] > id.classList[0]) {
+                elements[i].classList.add("d-none");
+            }
+        }
+        document.getElementById(e.target.name).classList.remove("d-none");
     }
   return (
-      <div>
-          <h3>{value.value}</h3>
-          <div>
-              <button className="btn btn-primary w-25" value="oui" onClick={(e) => handleInput(e, 'value')}>Oui</button>
-              <button className="btn btn-primary w-25" value="non" onClick={(e) => handleInput(e, 'value')}>Non</button>
+      <>
+          <div className={props.question[question][0].class} id={props.question[question][0].id}>
+            <h3>{props.question[question][0].label}</h3>
+            {props.question[question][0].answer ?
+              <div>
+                  {props.question[question][0].answer.map((answer, index) => {
+                      return (
+                          <button key={index} className="btn btn-primary w-25" value={answer.label} name={answer.to}
+                      onClick={e => handleInput(e, "value")}>{answer.label}</button>
+                      )
+                  })}
+              </div>
+              :
+              null
+            }
           </div>
-      </div>
+      </>
   );
 }
-export default Test;
+export default Questions;
